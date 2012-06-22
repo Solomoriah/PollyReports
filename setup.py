26de9eb5c29b63864c814e05f19025a6dbc5608c
Chris Gonnerman <chrisg@newcenturycomputers.net>
2012-06-22 07:32:22 -0500

    Updated description in setup.py

--- setup.py
+++ setup.py
@@ -11,25 +11,18 @@ PollyReports.py provides a set of classes for database report writing.  It
 assumes that you are using Reportlab to do PDF generation, but can work with
 any "canvas-like" object as desired.
 
-PollyReports provides the following framework for report generation:
+PollyReports provides a framework for report generation.  The developer
+instantiates a Report object, passing it a data source and passing or assigning
+one or more Band objects.  A Band, in turn, will contain a list of Elements
+representing data items, labels, or system variables which will be printed.
+When the Report object is fully populated, its generate() method is called,
+passing in a Reportlab Canvas object (or any object providing a similar interface);
+the Report object then consumes the data source and renders the various Bands
+of Elements into the Canvas object.
 
-A Report object has a data source bound to it at instantiation.  One or more
-Band objects (at least, a detail Band) must be added to it, and then the
-generate() method will be called to process the data source.  The data source
-must be an iterator that produces objects that can be accessed via []
-operations, meaning mainly dict, list, and tuple types, i.e. the most common
-types of records returned by standard database modules.  The detail band is
-generated() once for each row.
-
-Band objects contain a list of Elements (generally at least one) which define
-how data from the row should be printed.  An Element may print any normal data
-item or label and may be subclassed to handle other things like images.
-Generating a band in turn calls Element.generate() for each element, producing
-a list of Renderers with the first item in the list being the overall height of
-the band.  The height is used to decide if the band will fit on the current
-page; if not, a new page will be created first.  When the page is finally ready
-for the band, Renderer.render() will be called for each Renderer in the element
-list in order to actually render the data.
+The data source must be an iterator that produces objects that can be accessed
+via [] operations, meaning mainly dict, list, and tuple types, i.e. the most
+common types of records returned by standard database modules.
 
 This version is written for Python 2.7, but should be easily adapted to Python 3.
 """
